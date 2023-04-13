d5dab7f055de2b72a09fa5101c8310cbe713a155
Marcos P. Gusmão <marcosp.gusmao@MacBook-Pro-de-Marcos-3.local>
2023-04-12 22:53:12 -0300

    Projeto Final

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { Tasks } from "./components/Tasks";
+
+import './global.css';
+
+export interface ITask {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
+export function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function addTask(taskTitle: string) {
+    setTasks([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false
+      }
+    ]);
+  }
+
+  function deleteTaskById(taskId: string) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function toggleTaskCompletedById(taskId: string) {
+    const newTasks = tasks.map(task => {
+      if(task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  return (
+    <div>
+      <Header onAddTask={addTask} />
+      <Tasks 
+        tasks={tasks} 
+        onDelete={deleteTaskById} 
+        onComplete={toggleTaskCompletedById}
+      />
+
+    </div>
+  )
+}
+
+
+
+
